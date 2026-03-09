1b8a3c06ec4d0e762d5efaf8f256e08b8ec8da81
Sebastian Booth <ItzBidAhs@gmail.com>
2026-03-09 11:12:36 -0700

    Increase plane data limit from 175 to 350

--- api/planes.js
+++ api/planes.js
@@ -12,7 +12,7 @@ module.exports = async (req, res) => {
 
     const planes = aircraft
       .filter((p) => p.lat != null && p.lon != null) // Ensure lat/lon present
-      .slice(0, 175)
+      .slice(0, 350)
       .map((p) => ({
         callsign: (p.flight || p.callsign || "Unknown").trim(),
         lat: p.lat,

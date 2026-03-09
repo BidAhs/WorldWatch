ceae0314306a890ec10900c9ccbc661a43ab2036
Sebastian Booth <ItzBidAhs@gmail.com>
2026-03-09 11:53:49 -0700

    Round latitude and longitude to two decimal places

--- api/earthquakes.js
+++ api/earthquakes.js
@@ -6,8 +6,8 @@ module.exports = async (req, res) => {
       "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
     const { data } = await axios.get(url);
     const result = data.features.map((f) => ({
-      lat: f.geometry.coordinates[1],
-      lon: f.geometry.coordinates[0],
+      lat: Math.round(f.geometry.coordinates[1] * 100) / 100,
+      lon: Math.round(f.geometry.coordinates[0] * 100) / 100,
       mag: f.properties.mag,
     }));
     res.json(result);
